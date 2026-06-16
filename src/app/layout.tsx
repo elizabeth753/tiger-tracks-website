3d97bc32919ef4d752ef9965dba44c533845910a
Tiger Tracks <cliffminimac@gmail.com>
2026-06-16 12:25:14 -0400

    chore: restore real GTM container ID (GTM-TGZKSQMT)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -91,7 +91,7 @@ export default function RootLayout({
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-PLACEHOLDER');
+              })(window,document,'script','dataLayer','GTM-TGZKSQMT');
             `,
           }}
         />
@@ -156,7 +156,7 @@ export default function RootLayout({
         <WebVitals />
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PLACEHOLDER"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TGZKSQMT"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
